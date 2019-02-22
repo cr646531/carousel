e7237c99591f088799a55d5f9deb83681b0f101f
Charlie <charlie@HQ.fios-router.home>
2019-02-22 01:07:28 -0500

    troubleshooting heroku postgres

--- db/conn.js
+++ db/conn.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
-const conn = new Sequelize(process.env.DATABASE_URL);
+const conn = new Sequelize(process.env.DATABASE_URL,{
+    dialect: 'mysql'
+  });
 
 module.exports = conn;
